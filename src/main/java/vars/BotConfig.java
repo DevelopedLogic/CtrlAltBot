d6bf82c0a54ab93cee3a888fd95f62bd40a50dbd
DevelopedLogic <developing11@outlook.com>
2017-07-16 08:42:32 +0100

    Fixed gradle

--- src/main/java/vars/BotConfig.java
+++ src/main/java/vars/BotConfig.java
@@ -7,7 +7,7 @@ import net.dv8tion.jda.core.AccountType;
 public class BotConfig{
 	//Settings
 	public static final AccountType accType = AccountType.BOT;
-	public static final String token = "";
+	public static final String token = "MzM2MDM4MTI5ODEzMTU5OTM3.DEyfaA.wa5RrzQBL4H8SXokt650dgu17v4";
 	public static String prefix = ",";
 	public static boolean StateOnlinePresence = false;
 	public static boolean log = false;
