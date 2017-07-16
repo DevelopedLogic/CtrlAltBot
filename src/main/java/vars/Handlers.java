2106e762ec3dc2f751b099b9c1a92f8215c11bf4
DevelopedLogic <developing11@outlook.com>
2017-07-16 09:12:18 +0100

    Updates to dependencies

--- src/main/java/vars/Handlers.java
+++ src/main/java/vars/Handlers.java
@@ -2,13 +2,11 @@ package vars;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
 import net.dv8tion.jda.core.JDA;
 
 public class Handlers{
 	public static JFrame ui;
 	public static JDA jdaHandler;
 	public static JLabel uiOnlineStatus;
-	public static AudioPlayerManager lavaplayer;
 	public static boolean uiAllowed;
 }
